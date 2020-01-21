947c776d4d68cccf2231d1b5079a42e2f1cd3672
Damien BATTISTELLA <damien.battistella@agfa.com>
2020-01-21 10:51:43 +0100

    feat: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es6: true,
+    jest: true
+  },
+  parserOptions: {
+    sourceType: "module"
+  },
+  extends: ["eslint:recommended", "plugin:node/recommended"],
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "linebreak-style": "off", // don't matter line ending style
+    indent: ["error", 2], // indent with 2 spaces
+    quotes: ["error", "single"], // force single quotes
+    semi: ["error", "never"], // remove semicolons
+    eqeqeq: "warn", // require === and !==
+    "default-case": "warn", // require default case in switch statements
+    "no-implicit-coercion": "warn", // disallows implicit type conversion methods
+    "no-magic-numbers": "off",
+    yoda: "warn", // requires 'yoda' condition statements
+    "no-var": "warn", // requires let or const, not var
+    "node/no-missing-require": ["error", {
+      "allowModules": ["utils", "channels", "classes"]
+    }]
+  }
+};
---
+++ build/webpack.common.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const webpack = require("webpack")
+const nodeExternals = require("webpack-node-externals")
+
+module.exports = {
+  entry: [path.join(__dirname, '../src/server.js')],
+  target: "node",
+  externals: [nodeExternals()],
+  output: {
+    filename: "backend.js",
+    path: path.resolve("dist"),
+    publicPath: "dist/"
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      DIR_NAME: JSON.stringify("dist")
+    })
+  ],
+  resolve: {
+    modules: [path.join(__dirname, '../src/'), 'node_modules'],
+    alias: {
+      channels: path.join(__dirname, "../src/channels/"),
+      classes: path.join(__dirname, "../src/classes/"),
+      cron: path.join(__dirname, "../src/cron/"),
+      data: path.join(__dirname, "../src/data/"),
+      utils: path.join(__dirname, "../src/utils/")
+    }
+  }
+}
