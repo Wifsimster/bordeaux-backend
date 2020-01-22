eb1d45599b80cf9c045646406fa1a03e899b4050
Damien BATTISTELLA <damien.battistella@agfa.com>
2020-01-22 11:31:49 +0100

    fix: webpack build

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -1,29 +1,31 @@
 const path = require('path')
-const webpack = require("webpack")
-const nodeExternals = require("webpack-node-externals")
+const webpack = require('webpack')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const nodeExternals = require('webpack-node-externals')
 
 module.exports = {
   entry: [path.join(__dirname, '../src/server.js')],
-  target: "node",
+  target: 'node',
   externals: [nodeExternals()],
   output: {
-    filename: "backend.js",
-    path: path.resolve("dist"),
-    publicPath: "dist/"
+    filename: 'backend.js',
+    path: path.resolve('dist'),
+    publicPath: 'dist/'
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new webpack.DefinePlugin({
-      DIR_NAME: JSON.stringify("dist")
+      DIR_NAME: JSON.stringify('dist')
     })
   ],
   resolve: {
     modules: [path.join(__dirname, '../src/'), 'node_modules'],
     alias: {
-      channels: path.join(__dirname, "../src/channels/"),
-      classes: path.join(__dirname, "../src/classes/"),
-      cron: path.join(__dirname, "../src/cron/"),
-      data: path.join(__dirname, "../src/data/"),
-      utils: path.join(__dirname, "../src/utils/")
+      channels: path.join(__dirname, '../src/channels/'),
+      classes: path.join(__dirname, '../src/classes/'),
+      cron: path.join(__dirname, '../src/cron/'),
+      data: path.join(__dirname, '../src/data/'),
+      utils: path.join(__dirname, '../src/utils/')
     }
   }
 }
